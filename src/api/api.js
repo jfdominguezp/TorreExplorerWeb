bd8fef72a473247e391e6d3c1670dc1afe2d25be
Juan Domínguez <juan@buziapp.com>
2019-02-09 18:40:52 -0500

    Replace ordinal number function in PersonCard with Numeral package. Added default limit to the fetchConnections API call.

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,9 @@ import * as axios from 'axios';
 
 const API_URL = 'https://torre-explorer-api.herokuapp.com';
 
-export const fetchConnections =  async (publicId) => {
+export const fetchConnections =  async (publicId, limit = 20) => {
     try {
-        const response = await axios.get(`${API_URL}/top/${publicId}`);
+        const response = await axios.get(`${API_URL}/top/${publicId}?limit=${limit}`);
         return response.data;
     } catch (error) {
         console.log(error);
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -4,12 +4,6 @@ import _ from 'lodash';
 import * as numeral from 'numeral';
 
 export default class PersonCard extends Component {
-    getNumberWithOrdinal(n) {
-        var s=["th","st","nd","rd"],
-        v=n%100;
-        return n+(s[(v-20)%10]||s[v]||s[0]);
-    }
-
     render() {
         const { name, description, image, weight, index } = this.props;
         return (
@@ -18,7 +12,7 @@ export default class PersonCard extends Component {
                     <Image floated='left' size='mini' circular src={image} />
                     <Card.Header>{name}</Card.Header>
                     <Card.Meta>
-                        { `${this.getNumberWithOrdinal(index + 1)} in the rank - ${numeral(weight).format('0.0a')} reputation` }
+                        { `${numeral(index + 1).format('0o')} in the rank - ${numeral(weight).format('0.0a')} reputation` }
                     </Card.Meta>
                     <Card.Description className='card-description'>
                         { _.truncate(description, { length: 90 }) }
